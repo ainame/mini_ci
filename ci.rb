70fec50ba5a80040f3cdd5825366146dec2ebf99
satoshi.namai <satoshi.namai@mixi.co.jp>
2012-11-28 23:20:08 +0900

    バグ修正

--- ci.rb
+++ ci.rb
@@ -6,41 +6,41 @@ require 'date'
 require 'yaml'
 require 'tempfile'
 require 'active_support'
-require 'active_support/all'
-
-config = YAML.load(open('./setting.yaml').read)
-
-def build(base_dir, target_dir)
-  exit_status = false
-  error_locations = ''
-  Dir.chdir(base_dir) do
-    test_result_source = `#{config['test_command']} #{target_dir}`
-    exit_status        = $?.success?
-
-    Tempfile.open('./tmp_test_result_source.txt') do |f|
-      f.puts test_result_source
-      f.flush
-      serror_location = `perl ./test_error_location.pl < #{f.path}`
-    end
-  end
-
-  return [exit_status, error_locations]
-end
+require 'active_support/time'
 
 Chatroid.new do
-  set :service,  "Irc"
-  set :server,   config['server']
-  set :port,     config['port']
-  set :channel,  config['channel']
-  set :username, config['username']
-
+  @settings = YAML.load(open('./setting.yaml').read)
   @last_modified = DateTime.now
 
-  on_time :min => [0,5,10,15,20,25,30,35,40,45,50,55], :sec => 0 do
-    g = Git.new(config['base_dir'])
+  set :service,  "Irc"
+  set :server,   @settings['server']
+  set :port,     @settings['port']
+  set :channel,  @settings['channel']
+  set :username, @settings['username']
+  set :executed_dir, "#{Dir.pwd}"
+  
+  def build(base_dir, target_dir)
+    exit_status = false
+    error_locations = ''
+    Dir.chdir(base_dir) do
+      command = "#{@settings['test_command']} #{target_dir}"
+      test_result_source = `#{command}`
+      exit_status        = $?.success?
+      Tempfile.open('tmp') do |f|
+        f.puts test_result_source
+        f.flush
+        error_locations = `perl #{File.join(config[:executed_dir],'test_error_location.pl')} < #{f.path}`
+      end
+    end
+    
+    return [exit_status, error_locations]
+  end
+
+  on_time :sec => [0,5,10,15,20,25,30,35,40,45,50,55] do
+    g = Git.new(@settings['base_dir'])
     begin
       g.evaluate_with_base_dir do |git|
-        git.pull config['remote'], config['branch']
+        git.pull @settings['remote'], @settings['branch']
         git.parse_log(git.log('--after', 1.day.ago.to_date.to_s))
       end
     rescue
@@ -49,28 +49,33 @@ Chatroid.new do
 
     # last_modifiedと比較する処理
     if g.blobs.first.respond_to?(:date) && g.blobs.first.date > @last_modified
-      result, error_locations = build(config['base_dir'], config['target_dir'])
+      result, error_locations = build(@settings['base_dir'], @settings['target_dir'])
       unless result
-        privmsg config['channel'], ":" + "@all Failure!!!"
+        privmsg @settings['channel'], ":" + "@all Failure!!!"
 
         error_locations.each_line do |line|
-          notice config['channel'], ":" + line
+          notice @settings['channel'], ":" + line
         end
 
-        notice config['channel'], ":" + "--this cause these commits--"
+        notice @settings['channel'], ":" + "--this cause these commits--"
         commits = []
         g.blobs.each do |blob|
-          next unless blob.date <= @last_modified
-          notice config['channel'], ":" + "#{blob.author}: #{blob.message}"
+          if blob.date <= @last_modified
+            commits.push blob.commit
+            break
+          end
+
+          message = "#{blob.author.match(/\s*(.*)\s*<.*>/).captures[0].strip}: #{blob.message.strip}"
+          notice @settings['channel'], ":" + message
           commits.push blob.commit
         end
 
-        if config[:git_web_base_url]
-          commitdiff_url = "#{config[:git_web_base_url]}a=commitdiff&h=#{commits.first}&"
-          commitdiff_url .= "hp=#{commits.last}" if commits.size > 1
-          notice config['channel'], ":  " + commitdiff_url
+        if @settings['git_web_base_url']
+          commitdiff_url = "#{@settings['git_web_base_url']}a=commitdiff&h=#{commits.first}&"
+          commitdiff_url += "hp=#{commits.last}" if commits.size > 1
+          notice @settings['channel'], ":" + commitdiff_url
         else
-          notice config['channel'], ":  " + "#{commits.last}..#{commits.first}"
+          notice @settings['channel'], ":" + "#{commits.last}..#{commits.first}"
         end
         
       else
